957ae8a2796202f0594a3639089e1e146cbcfa74
Karan Yadav <xpresskaran98@gmail.com>
2021-01-07 11:27:46 +0530

    Added New templates, Python files , Static files

--- tut2.py
+++ tut2.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
 
 @app.route('/about')
-def karan():
+def about():
     name="karan"
     return render_template('about.html', name2=name)
+
+@app.route('/bootstrap')
+def bootstrap():
+    return render_template('bootstrap.html')
 
 app.run(debug=True)
